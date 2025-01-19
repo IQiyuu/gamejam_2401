ea8636ba8209502ee320533ad42008896862068d
Shadwogg <g>
2025-01-19 20:30:00 +0100

    Checkpoint and death added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,8 +18,11 @@ public class Player : MonoBehaviour {
     [SerializeField] AudioClip right;
     [SerializeField] AudioClip left;
 
+	Vector2 last_checkpoint;
+
     void Start() {
         Selected = gameObject.GetComponent<PlayerMovement>().runes;
+		last_checkpoint = transform.position;
     }
 
     void Update() {
@@ -28,6 +31,7 @@ public class Player : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.Q))
             PrecRune();
     }
+
     public void NextRune() {
         if (Runes.Count == 0)
             return ;
@@ -69,17 +73,32 @@ public class Player : MonoBehaviour {
         }
     }
 
+	public void	die()
+	{
+		transform.position = last_checkpoint;
+		Debug.Log("Player died");
+	}
+
     void OnTriggerStay2D( Collider2D coll ) {
         if (coll.CompareTag("Rune") && Input.GetKeyDown(KeyCode.F)) {
             Runes.Add(coll.GetComponent<Id>().id);
             Destroy(coll.gameObject);
             if (Rune_Index == 0)
                 Rune_Index = Runes[0];
+			last_checkpoint = coll.transform.position;
         }
 
         if (coll.CompareTag("Quest_Object") && Input.GetKeyDown(KeyCode.F)) {
             Quest_Objects.Add(coll.GetComponent<Id>().id);
             Destroy(coll.gameObject);
         }
+
+		if (coll.CompareTag("Enemy")) {
+			die();
+		}
+
+		if (coll.CompareTag("Checkpoint")){
+			last_checkpoint = coll.transform.position;
+		}
     }
 }
